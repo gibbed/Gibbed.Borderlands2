45938545d61abb19a31c10d1f5516fc13471c6c7
Rick Gibbed <gibbed@users.noreply.github.com>
2012-10-12 21:32:04 +0000

    Add build_distributable_saveedit.bat & co.

---
+++ dist/build_distributable_saveedit.bat
@@ -0,0 +1,15 @@
+@echo off
+rmdir /s /q "saveedit"
+mkdir "saveedit"
+msbuild "..\Borderlands 2.sln" /p:Configuration="SaveEdit Packaging" /p:OutputPath="%cd%\saveedit"
+copy /y "Gibbed.Borderlands2.SaveEdit.exe.config" "saveedit\Gibbed.Borderlands2.SaveEdit.exe.config"
+copy /y "..\license.txt" "saveedit\license.txt"
+copy /y "..\readme.txt" "saveedit\readme.txt"
+cd "saveedit"
+rem mkdir pdbs
+rem move *.pdb pdbs\
+del *.pdb
+mkdir assemblies
+move *.dll assemblies\
+del *.xml
+cd ".."
