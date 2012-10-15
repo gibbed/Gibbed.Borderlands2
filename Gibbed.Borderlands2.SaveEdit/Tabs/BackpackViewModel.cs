0e7dfde87bbcff0c33b5ed530b8c60b3e35f0dbb
Rick Gibbed <gibbed@users.noreply.github.com>
2012-10-15 04:30:01 +0000

    Implement duplicate, delete, copy code, paste code in BackpackView.

--- Gibbed.Borderlands2.SaveEdit/Tabs/BackpackViewModel.cs
+++ Gibbed.Borderlands2.SaveEdit/Tabs/BackpackViewModel.cs
@@ -21,11 +21,21 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.Composition;
+using System.Globalization;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
 using Caliburn.Micro;
+using Caliburn.Micro.Contrib;
+using Caliburn.Micro.Contrib.Dialogs;
+using Caliburn.Micro.Contrib.Results;
 using Gibbed.Borderlands2.FileFormats.Items;
+using PlayerMark = Gibbed.Borderlands2.ProtoBufFormats.WillowTwoSave.PlayerMark;
+using QuickWeaponSlot = Gibbed.Borderlands2.ProtoBufFormats.WillowTwoSave.QuickWeaponSlot;
 
 namespace Gibbed.Borderlands2.SaveEdit
 {
@@ -67,7 +77,8 @@ namespace Gibbed.Borderlands2.SaveEdit
         {
             var weapon = new BackpackWeapon()
             {
-                UniqueId = new Random().Next(int.MinValue, int.MaxValue), // TODO: check other item unique IDs to prevent rare collisions
+                UniqueId = new Random().Next(int.MinValue, int.MaxValue),
+                // TODO: check other item unique IDs to prevent rare collisions
                 AssetLibrarySetId = 0,
             };
             var viewModel = new BackpackWeaponViewModel(weapon);
@@ -79,12 +90,156 @@ namespace Gibbed.Borderlands2.SaveEdit
         {
             var item = new BackpackItem()
             {
-                UniqueId = new Random().Next(int.MinValue, int.MaxValue), // TODO: check other item unique IDs to prevent rare collisions
+                UniqueId = new Random().Next(int.MinValue, int.MaxValue),
+                // TODO: check other item unique IDs to prevent rare collisions
                 AssetLibrarySetId = 0,
             };
             var viewModel = new BackpackItemViewModel(item);
             this.Slots.Add(viewModel);
             this.SelectedSlot = viewModel;
+        }
+
+        private static readonly Regex _CodeSignature =
+            new Regex(@"BL2\((?<data>(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?)\)",
+                      RegexOptions.CultureInvariant | RegexOptions.Multiline);
+
+        public IEnumerable<IResult> PasteCode()
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text) != true &&
+                Clipboard.ContainsText(TextDataFormat.UnicodeText) != true)
+            {
+                yield break;
+            }
+
+            var errors = 0;
+            var viewModels = new List<IBackpackSlotViewModel>();
+            yield return new DelegateResult(() =>
+            {
+                var codes = Clipboard.GetText();
+
+                // strip whitespace
+                codes = Regex.Replace(codes, @"\s+", "");
+
+                foreach (var match in _CodeSignature.Matches(codes).Cast<Match>()
+                    .Where(m => m.Success == true))
+                {
+                    var code = match.Groups["data"].Value;
+
+                    IPackable packable;
+                    try
+                    {
+                        var data = Convert.FromBase64String(code);
+                        packable = BackpackDataHelper.Decode(data);
+                    }
+                    catch (Exception)
+                    {
+                        errors++;
+                        continue;
+                    }
+
+                    if (packable is BackpackWeapon)
+                    {
+                        var weapon = (BackpackWeapon)packable;
+                        weapon.QuickSlot = QuickWeaponSlot.None;
+                        weapon.Mark = PlayerMark.Standard;
+                        var viewModel = new BackpackWeaponViewModel(weapon);
+                        viewModels.Add(viewModel);
+                    }
+                    else if (packable is BackpackItem)
+                    {
+                        var item = (BackpackItem)packable;
+                        item.Quantity = 1;
+                        item.Equipped = false;
+                        item.Mark = PlayerMark.Standard;
+                        var viewModel = new BackpackItemViewModel(item);
+                        viewModels.Add(viewModel);
+                    }
+                }
+            });
+
+            if (viewModels.Count > 0)
+            {
+                viewModels.ForEach(vm => this.Slots.Add(vm));
+                this.SelectedSlot = viewModels.First();
+            }
+
+            if (errors > 0)
+            {
+                yield return
+                    new Warning("Warning",
+                                "Failed to load " + errors.ToString(CultureInfo.InvariantCulture) + " codes.",
+                                Answer.Ok).AsResult();
+            }
+        }
+
+        public void CopySelectedSlotCode()
+        {
+            if (this.SelectedSlot == null ||
+                !(this.SelectedSlot.Slot is IPackable))
+            {
+                Clipboard.SetText("", TextDataFormat.Text);
+                return;
+            }
+
+            var data = BackpackDataHelper.Encode((IPackable)this.SelectedSlot.Slot);
+            var sb = new StringBuilder();
+            sb.Append("BL2(");
+            sb.Append(Convert.ToBase64String(data, Base64FormattingOptions.None));
+            sb.Append(")");
+            Clipboard.SetText(sb.ToString(), TextDataFormat.Text);
+        }
+
+        public void DuplicateSelectedSlot()
+        {
+            if (this.SelectedSlot == null)
+            {
+                return;
+            }
+
+            var copy = (IBackpackSlot)this.SelectedSlot.Slot.Clone();
+            copy.UniqueId = new Random().Next(int.MinValue, int.MaxValue);
+            // TODO: check other item unique IDs to prevent rare collisions
+
+            if (copy is BackpackWeapon)
+            {
+                var weapon = (BackpackWeapon)copy;
+                weapon.QuickSlot = QuickWeaponSlot.None;
+                weapon.Mark = PlayerMark.Standard;
+
+                var viewModel = new BackpackWeaponViewModel(weapon);
+                this.Slots.Add(viewModel);
+                this.SelectedSlot = viewModel;
+            }
+            else if (copy is BackpackItem)
+            {
+                var item = (BackpackItem)copy;
+                item.Equipped = false;
+                item.Mark = PlayerMark.Standard;
+
+                var viewModel = new BackpackItemViewModel(item);
+                this.Slots.Add(viewModel);
+                this.SelectedSlot = viewModel;
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public void BankSelectedSlot()
+        {
+            // TODO: implement me
+        }
+
+        public void DeleteSelectedSlot()
+        {
+            if (this.SelectedSlot == null)
+            {
+                return;
+            }
+
+            this.Slots.Remove(this.SelectedSlot);
+            this.SelectedSlot = this.Slots.FirstOrDefault();
         }
 
         public void Handle(SaveUnpackMessage message)
