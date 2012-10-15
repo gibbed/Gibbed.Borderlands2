ba1f37938e509a56cea1afc9121d8707e1e04c8d
rick <rick@847a1a71-5502-e211-98e1-0030489379b8>
2012-10-15 11:58:38 +0000

    dist updates.

--- trunk/dist/build_distributable_saveedit.bat
+++ trunk/dist/build_distributable_saveedit.bat
@@ -5,6 +5,7 @@ msbuild "..\Borderlands 2.sln" /p:Configuration="SaveEdit Packaging" /p:OutputPa
 copy /y "Gibbed.Borderlands2.SaveEdit.exe.config" "saveedit\Gibbed.Borderlands2.SaveEdit.exe.config"
 copy /y "..\license.txt" "saveedit\license.txt"
 copy /y "..\readme.txt" "saveedit\readme.txt"
+svn log ".." > "saveedit\revisions.txt"
 cd "saveedit"
 rem mkdir pdbs
 rem move *.pdb pdbs\
@@ -12,4 +13,5 @@ del *.pdb
 mkdir assemblies
 move *.dll assemblies\
 del *.xml
+7z a -r -tzip -mx=9 ..\saveedit.zip .
 cd ".."
