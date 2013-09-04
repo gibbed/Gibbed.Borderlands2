0947034e24db13655a8b137c043e2a10463cdbb3
Rick Gibbed <gibbed@users.noreply.github.com>
2013-09-04 09:11:30 +0000

    Build distributable copies using a clean clone rather than the parent directory.

--- dist/build_distributable_saveedit.bat
+++ dist/build_distributable_saveedit.bat
@@ -1,15 +1,15 @@
 @echo off
 call "%VS100COMNTOOLS%vsvars32.bat"
-svn revert "..\Gibbed.Borderlands2.SaveEdit\Version.cs"
-svn update ".."
-subwcrev ".." "..\Gibbed.Borderlands2.SaveEdit\Version.cs.template" "..\Gibbed.Borderlands2.SaveEdit\Version.cs"
+svn revert "clone\Gibbed.Borderlands2.SaveEdit\Version.cs"
+svn update "clone"
+subwcrev "clone" "clone\Gibbed.Borderlands2.SaveEdit\Version.cs.template" "clone\Gibbed.Borderlands2.SaveEdit\Version.cs"
 rmdir /s /q "saveedit"
 mkdir "saveedit"
-msbuild "..\Borderlands 2.sln" /p:Configuration="SaveEdit Packaging" /p:OutputPath="%cd%\saveedit"
+msbuild "clone\Borderlands 2.sln" /p:Configuration="SaveEdit Packaging" /p:OutputPath="%cd%\saveedit"
 copy /y "Gibbed.Borderlands2.SaveEdit.exe.config" "saveedit\Gibbed.Borderlands2.SaveEdit.exe.config"
-copy /y "..\license.txt" "saveedit\license.txt"
-copy /y "..\readme.txt" "saveedit\readme.txt"
-svn log ".." > "saveedit\revisions.txt"
+copy /y "clone\license.txt" "saveedit\license.txt"
+copy /y "clone\readme.txt" "saveedit\readme.txt"
+svn log "clone" > "saveedit\revisions.txt"
 cd "saveedit"
 rem mkdir pdbs
 rem move *.pdb pdbs\
@@ -19,3 +19,4 @@ move *.dll assemblies\
 del *.xml
 7z a -r -tzip -mx=9 ..\saveedit.zip .
 cd ".."
+pause
