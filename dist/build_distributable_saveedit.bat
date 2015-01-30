00900f8a86806bac2d0aaa5ab9366cb8d03800a1
Rick Gibbed <gibbed@users.noreply.github.com>
2015-01-30 09:30:13 +0000

    - Corrected LZO support on 64-bit. (Stack corruption, ho! How did this ever work before?) - Updated LZO to 2.08. - Fixed distribution building for Any CPU.

--- dist/build_distributable_saveedit.bat
+++ dist/build_distributable_saveedit.bat
@@ -5,7 +5,7 @@ svn update "clone"
 subwcrev "clone" "clone\Gibbed.Borderlands2.SaveEdit\Version.cs.template" "clone\Gibbed.Borderlands2.SaveEdit\Version.cs"
 rmdir /s /q "saveedit"
 mkdir "saveedit"
-msbuild "clone\Borderlands 2.sln" /p:Configuration="SaveEdit Packaging" /p:OutputPath="%cd%\saveedit"
+msbuild "clone\Borderlands 2.sln" /p:Configuration="SaveEdit Packaging" /p:Platform="Any CPU" /p:OutputPath="%cd%\saveedit"
 copy /y "Gibbed.Borderlands2.SaveEdit.exe.config" "saveedit\Gibbed.Borderlands2.SaveEdit.exe.config"
 copy /y "clone\license.txt" "saveedit\license.txt"
 copy /y "clone\readme.txt" "saveedit\readme.txt"
@@ -17,6 +17,7 @@ del *.pdb
 mkdir assemblies
 move *.dll assemblies\
 del *.xml
+del ..\saveedit.zip
 7z a -r -tzip -mx=9 ..\saveedit.zip .
 cd ".."
 pause
